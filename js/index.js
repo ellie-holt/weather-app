dcd5738d7a11896f4adf6c44f7955b3036fa22b5
Ellie <eleanorholt97@gmail.com>
2023-05-31 16:16:59 +0100

    Added wind speed unit conversion

--- js/index.js
+++ js/index.js
@@ -55,11 +55,19 @@ function showWeather(response) {
   toCelciusButton.classList.add("inactive");
   toFarenheitButton.classList.remove("inactive");
 
+  metrespersecondWindSpeed = response.data.wind.speed;
+
+  //same as above
+  let currentWindUnitElement = document.querySelector("#wind-unit");
+  currentWindUnitElement.innerHTML = " m/s";
+  toMetrespersecondButton.classList.add("inactive");
+  toMphButton.classList.remove("inactive");
+
   let currentTemp = Math.round(celciusTemperature);
   let maxTemp = Math.round(response.data.main.temp_max);
   let minTemp = Math.round(response.data.main.temp_min);
   let humidity = `${response.data.main.humidity}%`;
-  let wind = Math.round(response.data.wind.speed * 10) / 10;
+  let wind = Math.round(metrespersecondWindSpeed * 10) / 10;
   let description = response.data.weather[0].description;
   let iconCode = response.data.weather[0].icon;
 
@@ -130,25 +138,32 @@ function toCelcius(event) {
   currentTempUnitElement.innerHTML = "°C";
 }
 
-// function convertTemp(event) {
-//   event.preventDefault();
-//   let convertTempButton = document.querySelector("#convert-temp");
-//   let tempValue = document.querySelector("#temp-value");
-//   let tempUnit = document.querySelector("#temp-unit");
-//   let temperature = tempValue.innerHTML;
-//   console.log(temperature);
-//   let farenheitTemp = Math.round(temperature * 1.8 + 32);
-//   let celciusTemp = Math.round((temperature - 32) / 1.8);
-//   if (tempUnit.innerHTML === "°C") {
-//     convertTempButton.innerHTML = "°C";
-//     tempValue.innerHTML = farenheitTemp;
-//     tempUnit.innerHTML = "°F";
-//   } else {
-//     convertTempButton.innerHTML = "°F";
-//     tempValue.innerHTML = celciusTemp;
-//     tempUnit.innerHTML = "°C";
-//   }
-// }
+function toMph(event) {
+  event.preventDefault();
+  let currentWindValueElement = document.querySelector("#wind-value");
+  let currentWindUnitElement = document.querySelector("#wind-unit");
+
+  let mphWindSpeed = metrespersecondWindSpeed * 2.237;
+
+  toMetrespersecondButton.classList.remove("inactive");
+  toMphButton.classList.add("inactive");
+
+  currentWindValueElement.innerHTML = Math.round(mphWindSpeed * 10) / 10;
+  currentWindUnitElement.innerHTML = " mph";
+}
+
+function toMetrespersecond(event) {
+  event.preventDefault();
+  let currentWindValueElement = document.querySelector("#wind-value");
+  let currentWindUnitElement = document.querySelector("#wind-unit");
+
+  toMphButton.classList.remove("inactive");
+  toMetrespersecondButton.classList.add("inactive");
+
+  currentWindValueElement.innerHTML =
+    Math.round(metrespersecondWindSpeed * 10) / 10;
+  currentWindUnitElement.innerHTML = " m/s";
+}
 
 let now = new Date();
 let fullDate = document.querySelector("#full-date");
@@ -159,6 +174,7 @@ currentTime.innerHTML = formatTime(now);
 navigator.geolocation.getCurrentPosition(getPosition);
 
 let celciusTemperature = null;
+let metrespersecondWindSpeed = null;
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchCity);
@@ -171,6 +187,12 @@ toFarenheitButton.addEventListener("click", toFarenheit);
 
 let toCelciusButton = document.querySelector("#celcius-button");
 toCelciusButton.addEventListener("click", toCelcius);
+
+let toMphButton = document.querySelector("#mph-button");
+toMphButton.addEventListener("click", toMph);
+
+let toMetrespersecondButton = document.querySelector("#metrespersecond-button");
+toMetrespersecondButton.addEventListener("click", toMetrespersecond);
 
 // function toFarenheit(event) {
 //   event.preventDefault();
@@ -190,4 +212,24 @@ toCelciusButton.addEventListener("click", toCelcius);
 //   convertTempButton.innerHTML = "°F";
 //   tempValue.innerHTML = Math.round((temperature - 32) / 1.8);
 //   tempUnit.innerHTML = "°C";
+// }
+
+// function convertTemp(event) {
+//   event.preventDefault();
+//   let convertTempButton = document.querySelector("#convert-temp");
+//   let tempValue = document.querySelector("#temp-value");
+//   let tempUnit = document.querySelector("#temp-unit");
+//   let temperature = tempValue.innerHTML;
+//   console.log(temperature);
+//   let farenheitTemp = Math.round(temperature * 1.8 + 32);
+//   let celciusTemp = Math.round((temperature - 32) / 1.8);
+//   if (tempUnit.innerHTML === "°C") {
+//     convertTempButton.innerHTML = "°C";
+//     tempValue.innerHTML = farenheitTemp;
+//     tempUnit.innerHTML = "°F";
+//   } else {
+//     convertTempButton.innerHTML = "°F";
+//     tempValue.innerHTML = celciusTemp;
+//     tempUnit.innerHTML = "°C";
+//   }
 // }
